a5468f54d8a359ba4b501975059dfb93f44cbfa2
erwanriou <riou@viamundus.com>
2018-02-22 16:43:53 +0100

    async search function implemented

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -5,7 +5,7 @@ const client_id = "03317009f2e84c71ae43443aa5848b8a";
 
 //we Create the acces token variable here. We will use it in the function below.
 let accessToken;
-
+let expiresIn;
 
 // Spotify API that regroup the 3 function token, GET and POST.
 const Spotify = {
@@ -21,7 +21,7 @@ const Spotify = {
     // step 80 condition statement on accesoken and expiration
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
+      expiresIn = Number(expiresInMatch[1]);
 
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
@@ -35,39 +35,33 @@ const Spotify = {
     }
   },
   // The second method that is the GET
-  search(term) {
-    const accessToken = Spotify.getAccessToken();
-    // I go here with the async function since i like his format better.
-    return async function getData() {
-      try {
-        let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          });
-        if (response.ok) {
-          let jsonResponse = await response.json();
-          if (!jsonResponse.tracks) {
-            return [];
-          } else {
-            return jsonResponse.tracks.item.map(track => {
-              return {
-                id: track.id,
-                name: track.name,
-                artist: track.artists[0].name,
-                album: track.album.name,
-                uri: track.uri
-              }
-            });
+  async search(term) {
+    if(accessToken === undefined) {
+				this.getAccessToken();
+			}
+    try {
+      let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`
           }
-        }
-        throw new Error('Request failed!');
-      } catch(error) {
-        console.log(error)
-      }
-    };
-
+        });
+      if (response.ok) {
+        let jsonResponse = await response.json();
+   			let tracks = jsonResponse.tracks.items.map(track => ({
+   				id: track.id,
+   				name: track.name,
+   				artist: track.artists[0].name,
+   				album: track.album.name,
+   				uri: track.uri
+   			}));
+   			return tracks;
+ 			}
+      throw new Error('Request failed!');
+    } catch(error) {
+      console.log(error);
+    }
   }
 }
 
